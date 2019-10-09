c5a6310cc76df5e8a846aa195b320208aa810c6c
Austin <rockmanvnx6@gmail.com>
2019-10-09 19:01:31 +1100

    Initialise with create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Login from './components/Login'
+import './App.css';
+
+class App extends Component{
+    render() {
+        return <div className="container">
+            <Login />
+        </div>
+    }
+}
+
+export default App;
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './styles/Login.scss'
+export default class Login extends Component {
+    render() {
+        return <div className="container">
+            <div className="row">
+                <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div className="card card-signin my-5">
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Sign In</h5>
+                            <form className="form-signin">
+                                <div className="form-label-group">
+                                    <input type="email" id="inputEmail" className="form-control"
+                                           placeholder="Email address" required autoFocus />
+                                        <label htmlFor="inputEmail">Email address</label>
+                                </div>
+
+                                <div className="form-label-group">
+                                    <input type="password" id="inputPassword" className="form-control"
+                                           placeholder="Password" required />
+                                        <label htmlFor="inputPassword">Password</label>
+                                </div>
+
+                                <div className="custom-control custom-checkbox mb-3">
+                                    <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+                                        <label className="custom-control-label" htmlFor="customCheck1">Remember
+                                            password</label>
+                                </div>
+                                <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign
+                                    in
+                                </button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+}
+
