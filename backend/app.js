01084a6cce1f42c9573b4613b5521c0bf7fec909
Austin <rockmanvnx6@gmail.com>
2019-10-09 16:09:31 +1100

    add user schema to register

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const app = express();
 const Database = require("mongoose");
-const authRoute = require('./routes/AuthorizeRoute');
 const log = require("morgan");
 const environment = require('dotenv');
 
+const bodyParser = require("body-parser");
+const authRoute = require('./routes/AuthorizeRoute');
+const app = express();
+
 environment.config();
 
-Database.connect(process.env.DB_CONNECT, { useNewUrlParser: true});
+Database.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }).then(
+        () => {
+            console.log("Connected to Database")
+        }
+    );
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(log("dev"));
 
--- backend/routes/AuthorizeRoute.js
+++ backend/routes/AuthorizeRoute.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-router.post('/register', (req,res) => {
-    res.send('Test');
-})
+router.post('/register', async (req,res) => {
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
 
 module.exports = router;
