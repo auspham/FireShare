a29d2230c202df9288b3158607c8e7234c3438db
Austin <rockmanvnx6@gmail.com>
2019-10-10 17:12:17 +1100

    Frontend Table

--- frontend/src/components/Main/Dashboard.js
+++ frontend/src/components/Main/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AccountService from '../../api/AccountService'
 import AuthenticationService from "../../api/AuthenticationService";
+import './styles/Dashboard.scss'
+import {Button, Table} from "react-bootstrap";
 export default class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -21,6 +23,45 @@ export default class Dashboard extends Component {
     }
 
     render() {
-        return <h1>{this.state.message}</h1>
+        return <div className="container mt-5">
+            <div className="table-head">
+                <div class="pull-left">
+                    <p>My File</p>
+                </div>
+                <div className="pull-right">
+                    <Button className="mb-2 float-right">Upload file</Button>
+                </div>
+                <div className="clearfix"></div>
+            </div>
+
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>File ID</th>
+                    <th>File name</th>
+                    <th>Modified</th>
+                    <th>Owner</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>1 min</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>2 min</td>
+                    <td>@fat</td>
+                </tr>
+
+                </tbody>
+            </Table>
+        </div>
     }
 }
