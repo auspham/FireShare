bf4869c97f743f53eb5560227ba319e46704c674
Austin <rockmanvnx6@gmail.com>
2019-10-13 18:16:30 +1100

    Updates

---
+++ precache-manifest.e5a5f5428bb90ff22521ae553d78a440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da00aca896cabd0ab938d296f2bfcdfd",
+    "url": "/FireShare/index.html"
+  },
+  {
+    "revision": "79ed7fe1c19f627068aa",
+    "url": "/FireShare/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "234686a36b99c3e5e2ff",
+    "url": "/FireShare/static/css/main.c299fa2a.chunk.css"
+  },
+  {
+    "revision": "79ed7fe1c19f627068aa",
+    "url": "/FireShare/static/js/2.d3e9f4ea.chunk.js"
+  },
+  {
+    "revision": "234686a36b99c3e5e2ff",
+    "url": "/FireShare/static/js/main.2df797bf.chunk.js"
+  },
+  {
+    "revision": "fb30ce0a38f145d92345",
+    "url": "/FireShare/static/js/runtime-main.f5d18282.js"
+  },
+  {
+    "revision": "4e0d68aeb617ba380fa86944f430a9e7",
+    "url": "/FireShare/static/media/loading.4e0d68ae.svg"
+  }
+]);
